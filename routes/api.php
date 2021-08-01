4150f757278c577d5a28fd6ca28ff8f6e64e4ca4
noruzzamanrubel <noruzzamanrubel@gmail.com>
2021-08-01 15:42:59 +0600

    resourseful controller create

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Buyer\BuyerController;
+use App\Http\Controllers\Category\CategoryController;
+use App\Http\Controllers\Product\ProductController;
+use App\Http\Controllers\Seller\SellerController;
+use App\Http\Controllers\Transaction\TransactionController;
+use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,8 +17,40 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+/**
+ * Buyers
+ */
+
+Route::resource( 'buyers', BuyerController::class )->only( ['index', 'show'] );
+
+/**
+ * Categorys
+ */
+
+Route::resource( 'categories', CategoryController::class )->except( ['create', 'edit'] );
+
+/**
+ * Products
+ */
+
+Route::resource( 'products', ProductController::class )->only( ['index', 'show'] );
+
+/**
+ * Sellers
+ */
+
+Route::resource( 'sellers', SellerController::class )->only( ['index', 'show'] );
+
+/**
+ * Transactions
+ */
+
+Route::resource( 'transactions', TransactionController::class )->only( ['index', 'show'] );
+
+/**
+ * Categorys
+ */
+
+Route::resource( 'users', UserController::class )->except( ['create', 'edit'] );
