4f145c0439f35f987d55769280b48389655bf95c
noruzzamanrubel <noruzzamanrubel@gmail.com>
2021-08-08 14:53:48 +0600

    install jwt package

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,7 +44,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
-        'varification_token',
+        // 'varification_token',
     ];
 
     /**
