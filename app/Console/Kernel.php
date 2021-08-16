820800ea5cfe507ef57aee9532327a4fe5c3929d
noruzzamanrubel <noruzzamanrubel@gmail.com>
2021-08-16 19:08:51 +0600

    task shedule add

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,11 +2,12 @@
 
 namespace App\Console;
 
+use App\Jobs\SendProfileCheckedMailJob;
+use App\Models\User;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
-class Kernel extends ConsoleKernel
-{
+class Kernel extends ConsoleKernel {
     /**
      * The Artisan commands provided by your application.
      *
@@ -22,9 +23,10 @@ class Kernel extends ConsoleKernel
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
-    protected function schedule(Schedule $schedule)
-    {
-        // $schedule->command('inspire')->hourly();
+    protected function schedule( Schedule $schedule ) {
+        $user = User::inRandomOrder()->first();
+        $schedule->job( new SendProfileCheckedMailJob( $user ) )->everyMinute();
+        $schedule->command( 'queue:work --stop-when-empty' )->everyMinute();
     }
 
     /**
@@ -32,10 +34,9 @@ class Kernel extends ConsoleKernel
      *
      * @return void
      */
-    protected function commands()
-    {
-        $this->load(__DIR__.'/Commands');
+    protected function commands() {
+        $this->load( __DIR__ . '/Commands' );
 
-        require base_path('routes/console.php');
+        require base_path( 'routes/console.php' );
     }
 }
