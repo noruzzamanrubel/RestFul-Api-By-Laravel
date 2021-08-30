c44d761fd4d7dedb5b3ff4e865dcb7c98212e04a
noruzzamanrubel <noruzzamanrubel@gmail.com>
2021-08-30 12:20:04 +0600

    update

--- routes/api.php
+++ routes/api.php
@@ -40,86 +40,98 @@ use Illuminate\Support\Facades\Route;
 |
  */
 
-Route::group( [
+Route::group([
     'middleware' => 'api',
-    'prefix'     => 'auth',
+    'prefix' => 'auth',
 
-], function ( $router ) {
-    Route::post( '/login', [AuthController::class, 'login'] );
-    Route::post( '/register', [AuthController::class, 'register'] );
-    Route::post( '/logout', [AuthController::class, 'logout'] );
-    Route::post( '/refresh', [AuthController::class, 'refresh'] );
-    Route::get( '/user-profile', [AuthController::class, 'userProfile'] );
-    Route::DELETE( '/delete/{id}', [AuthController::class, 'delete'] );
-} );
-
-Route::get( '/mail', function () {
-    
-    $user = User::inRandomOrder()->first();
-
-    SendTestJob::dispatch( $user )->delay( now()->addSeconds( 5 ) );
-
-    echo $user->name;
-} );
-
-Route::get( '/profilecheck', function () {
-
-    $user = User::inRandomOrder()->first();
-
-    SomeOneCheckedProfile::dispatch( $user );
-
-    echo $user->name . 'Your Profile Checked';
-} );
+], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);
+    Route::DELETE('/delete/{id}', [AuthController::class, 'delete']);
+});
 
 /**
  * Buyers
  */
 
-Route::resource( 'buyers', BuyerController::class )->only( ['index', 'show'] );
-Route::resource( 'buyers.transactions', BuyerTransactionController::class )->only( ['index'] );
-Route::resource( 'buyers.products', BuyerProductController::class )->only( ['index'] );
-Route::resource( 'buyers.sellers', BuyerSellerController::class )->only( ['index'] );
+Route::resource('buyers', BuyerController::class)->only(['index', 'show']);
+Route::resource('buyers.transactions', BuyerTransactionController::class)->only(['index']);
+Route::resource('buyers.products', BuyerProductController::class)->only(['index']);
+Route::resource('buyers.sellers', BuyerSellerController::class)->only(['index']);
 
 /**
  * Categorys
  */
 
-Route::resource( 'categories', CategoryController::class )->except( ['create', 'edit'] );
-Route::resource( 'categories.products', CategoryProductController::class )->only( ['index'] );
-Route::resource( 'categories.transactions', CategoryTransactionsController::class )->only( ['index'] );
-Route::resource( 'categories.sellers', CategorySellerController::class )->only( ['index'] );
-Route::resource( 'categories.buyers', CategoryBuyerController::class )->only( ['index'] );
+Route::resource('categories', CategoryController::class)->except(['create', 'edit']);
+Route::resource('categories.products', CategoryProductController::class)->only(['index']);
+Route::resource('categories.transactions', CategoryTransactionsController::class)->only(['index']);
+Route::resource('categories.sellers', CategorySellerController::class)->only(['index']);
+Route::resource('categories.buyers', CategoryBuyerController::class)->only(['index']);
 
 /**
  * Products
  */
 
-Route::resource( 'products', ProductController::class )->only( ['index', 'show'] );
-Route::resource( 'products.buyers', ProductBuyerController::class )->only( ['index'] );
-Route::resource( 'products.sellers', ProductSellerController::class )->only( ['index'] );
-Route::resource( 'products.transactions', ProductTransactionController::class )->only( ['index'] );
-Route::resource( 'products.categories', ProductCategoryController::class )->except( ['create', 'edit', 'store', 'show'] );
+Route::resource('products', ProductController::class)->only(['index', 'show']);
+Route::resource('products.buyers', ProductBuyerController::class)->only(['index']);
+Route::resource('products.sellers', ProductSellerController::class)->only(['index']);
+Route::resource('products.transactions', ProductTransactionController::class)->only(['index']);
+Route::resource('products.categories', ProductCategoryController::class)->except(['create', 'edit', 'store', 'show']);
 
 /**
  * Sellers
  */
 
-Route::resource( 'sellers', SellerController::class )->only( ['index', 'show'] );
-Route::resource( 'sellers.buyers', SellerBuyerColtroller::class )->only( ['index'] );
-Route::resource( 'sellers.transactions', SellerTransactionColtroller::class )->only( ['index'] );
-Route::resource( 'sellers.products', SellerProductColtroller::class )->only( ['index'] );
+Route::resource('sellers', SellerController::class)->only(['index', 'show']);
+Route::resource('sellers.buyers', SellerBuyerColtroller::class)->only(['index']);
+Route::resource('sellers.transactions', SellerTransactionColtroller::class)->only(['index']);
+Route::resource('sellers.products', SellerProductColtroller::class)->only(['index']);
 
 /**
  * Transactions
  */
 
-Route::resource( 'transactions', TransactionController::class )->only( ['index', 'show'] );
-Route::resource( 'transactions.sellers', TransactionSellerController::class )->only( ['index'] );
-Route::resource( 'transactions.products', TransactionProductController::class )->only( ['index'] );
-Route::resource( 'transactions.buyers', TransactionBuyerController::class )->only( ['index'] );
+Route::resource('transactions', TransactionController::class)->only(['index', 'show']);
+Route::resource('transactions.sellers', TransactionSellerController::class)->only(['index']);
+Route::resource('transactions.products', TransactionProductController::class)->only(['index']);
+Route::resource('transactions.buyers', TransactionBuyerController::class)->only(['index']);
 
 /**
  * Users
  */
 
-Route::resource( 'users', UserController::class )->except( ['create', 'edit'] );
+Route::resource('users', UserController::class)->except(['create', 'edit']);
+
+
+
+/**
+ *Practice
+ *
+ * Email and Notification
+ */
+Route::get('/mail', function () {
+
+    $user = User::inRandomOrder()->first();
+
+    SendTestJob::dispatch($user)->delay(now()->addSeconds(5));
+
+    echo $user->name;
+});
+
+/**
+ * Practice
+ *
+ * Profile check and Notification
+ */
+Route::get('/profilecheck', function () {
+
+    $user = User::inRandomOrder()->first();
+
+    SomeOneCheckedProfile::dispatch($user);
+
+    echo $user->name . 'Your Profile Checked';
+});
